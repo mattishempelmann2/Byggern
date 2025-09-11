e77473027410184ce204504720deca63d458848e
Mattis Joost Hempelmann <mattisjh@ntnu.no>
2025-09-11 15:52:41 +0200

    start of day2

---
+++ Byggern/SRAM.h
@@ -0,0 +1,12 @@
+/*
+ * IncFile2.h
+ *
+ * Created: 11.09.2025 15:26:01
+ *  Author: mattisjh
+ */ 
+
+#include <stdlib.h>
+
+void SRAM_init();
+
+void SRAM_test(void);
--- Byggern/main.c
+++ Byggern/main.c
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include "avr/iom162.h"
 #include "uart.h"
+#include "time.h"
 
 #define BAUD 9600 //Baud Rate
 #define FOSC 4915200 //Clock speed
@@ -15,23 +16,33 @@
 #define MYUBRR 31
 
 int main(void)
-{
-	//DDRA = 0xFF;
-	//PORTA |= (0<<PA7);
-	
+{	
     /* Replace with your application code */
-	UART_init(MYUBRR);
-	printf("Ting funker!");
+	//UART_init(MYUBRR);
+	//printf("Ting funker!");
+	DDRA = 0xFF;
+	DDRE |= (1<<PE1);
+	PORTA &= (0<<PA0);
+	
+// 		test av latch
+// 		//PORTE |= (1<<PE1);
+// 		PORTA |= (1<<PA0);
+// 		PORTE &= (0<<PE1);
+// 		PORTA &= (0<<PA0);
+	
+
+	
     while (1) 
     {
 		//UART_transmit('a');
+		//printf("Ting funker!");
 		//UART_transmit(UART_receive());
 		
 		//for(int n=0;n<10000;n++){
 			//PORTA |= (1<<PA7);
 		//}
 		//for(int n=0;n<10000;n++){
-		//	PORTA &= (0<<PA7);
+			//PORTA &= (0<<PA7);
 		//}
 	}
 }
