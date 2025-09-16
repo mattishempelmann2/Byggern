f57dd4d1ff905505cc02a80ab0ffb58963687e88
mattishempelmann2 <148902039+mattishempelmann2@users.noreply.github.com>
2025-09-16 14:40:10 +0200

    3kB SRAM fixed

--- Byggern/SRAM.c
+++ Byggern/SRAM.c
@@ -16,8 +16,8 @@ void SRAM_init(){
 }
 
 void SRAM_test(void) {
-	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
-	uint16_t ext_ram_size = 0x800;
+	volatile char *ext_ram = (char *) 0x1400; // Start address for the SRAM
+	uint16_t ext_ram_size = 0xC00;
 	uint16_t write_errors = 0;
 	uint16_t retrieval_errors = 0;
 	printf("Starting SRAM test...\n");
