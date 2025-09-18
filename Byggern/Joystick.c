9e5c797990eb21eabfd7818d305640633b0c3a7d
Johannes Knappskog <johankna@ntnu.no>
2025-09-18 15:42:56 +0200

    Added infrastructure for future implementations

---
+++ Byggern/Joystick.c
@@ -0,0 +1,6 @@
+/*
+ * Joystick.c
+ *
+ * Created: 18.09.2025 15:33:20
+ *  Author: johankna
+ */ 
---
+++ Byggern/Joystick.h
@@ -0,0 +1,16 @@
+/*
+ * Joystick.h
+ *
+ * Created: 18.09.2025 15:33:33
+ *  Author: johankna
+ */ 
+
+
+#ifndef JOYSTICK_H_
+#define JOYSTICK_H_
+
+
+
+
+
+#endif /* JOYSTICK_H_ */
