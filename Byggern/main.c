0f55ff774e4cf88a2a00f457fa7624922f912470
Johannes Knappskog <johankna@ntnu.no>
2025-09-18 14:08:48 +0200

    Working clock from ATMEGA

--- Byggern/main.c
+++ Byggern/main.c
@@ -15,18 +15,23 @@
 #define FOSC 4915200 //Clock speed
 //#define MYUBRR FOSC/(16*BAUD)-1
 #define MYUBRR 31
+#define target_hz FOSC/2
 
 int main(void)
 {	
     /* Replace with your application code */
-	UART_init(MYUBRR);
-  	SRAM_init();
-  	SRAM_test();
+	//UART_init(MYUBRR);
+  	//SRAM_init();
+  	//SRAM_test();
 	
 	//printf("Ting funker!");
 	
 	
 // 	DDRA = 0xFF;
+	DDRD |= (1<<PD4);
+	//OCR3A = 128;
+	ICR3 = 1;
+	OCR3A = 0;
 // 	DDRE |= (1<<PE1);
 // 	PORTA &= (0<<PA0);
 	
@@ -35,13 +40,14 @@ int main(void)
 // 		PORTA |= (1<<PA0);
 // 		PORTE &= (0<<PE1);
 // 		PORTA &= (0<<PA0);
-	
-	
+
+	TCCR3A |= (1<<COM3A1) | (1<<WGM31);
+	TCCR3B |= (1<<WGM33) | (1<<WGM32) | (1<<CS30);
 
 	
     while (1) 
     {
-		//UART_transmit('a');
+ 		//UART_transmit('a');
 		//printf("Ting funker!");
 		//UART_transmit(UART_receive());
 		
